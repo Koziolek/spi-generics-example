187a6c4021095e195fe14d12d4a5554654b090aa
Koziolek <bjkuczynski@gmail.com>
2022-10-20 14:09:56 +0200

    A tu się zesrało

--- client/src/main/java/pl/koziolekweb/spimagic/App.java
+++ client/src/main/java/pl/koziolekweb/spimagic/App.java
@@ -2,12 +2,17 @@ package pl.koziolekweb.spimagic;
 
 import pl.koziolekweb.spimagic.spi.Wrapper;
 
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
         Wrapper wrapper = WrapperProviderSpi.load().orElseThrow(() -> new RuntimeException("No impl provided"));
-        Object o = wrapper.sizeOf(wrapper.get());
+        List<Long> list = wrapper.get();
+        Integer o = wrapper.sizeOf(list);
         System.out.println(o.getClass());
+        Long aLong = list.get(0);
+        System.out.println(aLong);
     }
 
 }
--- spi/src/main/java/pl/koziolekweb/spimagic/spi/Wrapper.java
+++ spi/src/main/java/pl/koziolekweb/spimagic/spi/Wrapper.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface Wrapper {
 
-    List<Integer> get();
+    List<Long> get();
 
-    Long sizeOf(List<Integer> list);
+    Integer sizeOf(List<Long> list);
 }
