04140e4c4419226e747821511f007228dce7df0f
Koziolek <bjkuczynski@gmail.com>
2022-10-21 10:13:55 +0200

    usnięcie plików ide

--- client/src/main/java/pl/koziolekweb/spimagic/App.java
+++ client/src/main/java/pl/koziolekweb/spimagic/App.java
@@ -8,10 +8,10 @@ public class App {
 
     public static void main(String[] args) {
         Wrapper wrapper = WrapperProviderSpi.load().orElseThrow(() -> new RuntimeException("No impl provided"));
-        List<Integer> list = wrapper.get();
+        List<Long> list = wrapper.get();
         Object size = wrapper.sizeOf(list);
         System.out.println(size.getClass());
-        Object o = list.get(0);
+        Long o = list.get(0);
         System.out.println(o.getClass());
     }
 
